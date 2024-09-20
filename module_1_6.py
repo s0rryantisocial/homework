66c713493161b866dd9d1c721c53bf925e62013d
s0rryantisocial <1ydbeats@mail.ru>
2024-09-20 18:31:35 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'Sasha': 2002, 'Sema': 2014, 'Oleg': 2004, 'Sereja': 2012}
+print(my_dict)
+print(my_dict['Sereja'])
+my_dict.update({'Vasya': 2009, 'Petya': 1999})
+print(my_dict)
+my_dict.pop('Petya')
+print(my_dict)
+my_set = {1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 'Privet', True}
+print(my_set)
+my_set.add(9)
+my_set.add('vosem')
+print(my_set)
+my_set.remove('Privet')
+print(my_set)
