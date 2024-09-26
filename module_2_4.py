514a664a27eee6510a7fbf17ddc9157804382894
s0rryantisocial <1ydbeats@mail.ru>
2024-09-26 18:27:46 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for num in numbers:
+    if num == 1:
+        continue
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(num)
+    else:
+        not_primes.append(num)
+print("Простые числа:", primes)
+print("Не простые числа:", not_primes)
