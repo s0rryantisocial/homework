8de243f7ad9c56423f7532379deaa441759feea7
s0rryantisocial <1ydbeats@mail.ru>
2024-09-26 14:19:54 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while True:
+    if my_list[0] > 0:
+        print(my_list.pop(0))
+        continue
+    else:
+        my_list[0] < 1 
+        break
